604a65a967cd8a894398d2f127f4b0705c31dfc6
HarryPotter12352 <harrypottr12352.github@outlook.com>
2025-01-22 17:31:00 +0530

    New attempt

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use BrowserRouter
 import App from './App';
 import './index.css';
-import BlogNewYear from './blog/blognewyear'; // Import the blog page for routing
+import BlogNewYear from './blog/blognewyear'; // Import your BlogNewYear component
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}> {/* Ensure correct base path for GitHub Pages */}
+  <Router basename={process.env.PUBLIC_URL}> {/* Ensure this is set to handle subpaths */}
     <Routes>
-      {/* Route for the home page */}
       <Route path="/" element={<App />} />
-      
-      {/* Route for BlogNewYear page */}
       <Route path="/blognewyear" element={<BlogNewYear />} />
-      
-      {/* Add other routes as needed */}
+      {/* Add more routes as necessary */}
     </Routes>
   </Router>,
   document.getElementById('root')
