fe2016e2311d3194c408f875f5aa983fbd736451
HarryPotter12352 <harrypottr12352.github@outlook.com>
2025-01-22 17:01:34 +0530

    Attempt to fix routing errors

--- src/index.js
+++ src/index.js
@@ -2,27 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Hero from './Hero';
-import About from './About';
-import Experience from './Experience';
-import PublicationsSection from './Publications';
-import ContactSection from './Contact';
-import Navbar from './Navbar';
 import './index.css'; 
-import PhysicsUtilitiesSection from './tools';
+
 import BlogNewYear from './blog/blognewyear';
 
 // Render the App component wrapped with Router for routing capabilities
 ReactDOM.render(
 
-  <Router>
-    {/* <Navbar />
-    <Hero />
-    <About />
-    <Experience />
-    <PublicationsSection />
-    <PhysicsUtilitiesSection />
-    <ContactSection />  */}
+  <Router basename={process.env.PUBLIC_URL}>
     <Routes>
     <Route path = "/" element = {<App />}/>
     <Route path ="/blognewyear" element = {<BlogNewYear/>}/>
